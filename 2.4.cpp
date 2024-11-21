95a969d4c5e11a79a9b6b00ca45500c9898ee404
Gaabuto <khanh040604@gmail.com>
2024-11-21 08:06:48 +0700

    2.4

---
+++ 2.4.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+	int canhHinhVuong = 5, chuVi = 5 * 4, dienTich = 5 * 5;
+	printf("canh hinh vuong la %d\n", canhHinhVuong);
+	printf("chu vi hinh vuong la %d\n", chuVi); 
+	printf("dien tich ihnh vuong la %d", dienTich);
+	return 0;
+     
+	
+}
