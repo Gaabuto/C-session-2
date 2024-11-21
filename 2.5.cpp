4c58640f9bd282600e1a6f378b71d7fd1a3ba0b3
Gaabuto <khanh040604@gmail.com>
2024-11-21 08:07:00 +0700

    2.5

---
+++ 2.5.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+	int chieuDai = 5,chieuRong = 4, chuVi = (5+4)*2, dienTich = 5 * 4;
+	printf("chieu dai hinh vuong la %d, chieu rong hinh vuong la %d\n", chieuDai, chieuRong);
+	printf("chu vi hinh chu nhat la %d\n", chuVi); 
+	printf("dien tich hinh chu nhat la %d", dienTich);
+	return 0;
+     
+	
+}
